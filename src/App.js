36c5bab2f5cc404e9309b135f742ede9b83828d0
messmonte <amandamariamonte@gmail.com>
2020-03-28 21:36:39 +0100

    Added Homepage + styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { Route } from "react-router-dom";
 
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
 import AuthPage from "./components/AuthPage";
 import SignupFormContainer from "./components/SignupFormContainer";
-
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Route exact path="/" component={Homepage} />
       <Route exact path="/login" component={AuthPage} />
       <Route exact path="/signup" component={SignupFormContainer} />
     </div>
---
+++ src/components/Homepage.js
@@ -0,0 +1,28 @@
+import React from "react";
+// import { getEvents } from "../store/actions/event";
+// import { connect } from "react-redux";
+import "./Homepage.css";
+
+export default class Homepage extends React.Component {
+  componentDidMount() {}
+  render() {
+    return (
+      <div className="home">
+        <img className="homepage-img" src={require("../assets/images/bg.png")} />
+        <div className="description">
+          <h1>Why do you need LazyCook?</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            commodo ante ut elit porttitor, non hendrerit dolor ornare.
+            Suspendisse venenatis eros ac scelerisque aliquam. Maecenas ut odio
+            consequat, suscipit mi eu, efficitur leo. Pellentesque habitant
+            morbi tristique senectus et netus et malesuada fames ac turpis
+            egestas. Phasellus elementum vehicula erat. Praesent consectetur
+            velit urna, vitae dapibus justo ullamcorper nec. Sed bibendum nisi
+            in ipsum tempus, vitae sodales nisi fermentum.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
